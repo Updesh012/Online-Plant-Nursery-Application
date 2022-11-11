45d778e681f22055487824c1f3b1ca46b4676f67
Pritu2022 <candlepritu21century@gmail.com>
2022-11-11 23:24:20 +0530

    all extra exception handled

---
+++ The_Green_Country/src/main/java/com/masai/exceptions/InvalidKeyException.java
@@ -0,0 +1,15 @@
+package com.masai.exceptions;
+
+public class InvalidKeyException extends Exception {
+	
+	public InvalidKeyException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public InvalidKeyException(String errmsg) {
+		super(errmsg);
+	}
+	
+	
+
+}
