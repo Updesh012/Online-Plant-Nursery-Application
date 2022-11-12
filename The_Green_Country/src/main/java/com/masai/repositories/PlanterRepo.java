407ce2ed09a439f97a1297b13abfd6b804f417ef
Updesh <updeshkumarrajput01@gmail.com>
2022-11-12 11:57:10 +0530

    planter module completed!

--- The_Green_Country/src/main/java/com/masai/repositories/PlanterRepo.java
+++ The_Green_Country/src/main/java/com/masai/repositories/PlanterRepo.java
@@ -1,13 +1,18 @@
 package com.masai.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.masai.models.Planter;
+import com.masai.models.Seed;
 
 @Repository
 public interface PlanterRepo extends JpaRepository<Planter, Integer>{
 
-	
+	@Query("select p.seeds from Planter p where p.planterId = ?1")
+	public List<Seed> getAllSeedsByPlanterId(Integer id);
 	
 }
