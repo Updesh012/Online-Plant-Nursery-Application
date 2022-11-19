3628a975f5e3546782908c335390958b2d773722
Updesh <updeshkumarrajput01@gmail.com>
2022-11-19 10:25:44 +0530

    different request body class added

--- The_Green_Country/src/main/java/com/masai/TheGreenCountryApplication.java
+++ The_Green_Country/src/main/java/com/masai/TheGreenCountryApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@EnableSwagger2
 @SpringBootApplication
+@EnableSwagger2
 public class TheGreenCountryApplication {
 
 	public static void main(String[] args) {
